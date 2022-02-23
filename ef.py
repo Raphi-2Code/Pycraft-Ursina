6490d7420bdd0b533a1f5e3545ba64d6fbe54f9d
Raphi-2Code <70066593+Raphi-2Code@users.noreply.github.com>
2022-02-23 10:12:53 +0100

    Update ef.py

--- ef.py
+++ ef.py
@@ -4,9 +4,20 @@ from random import randint,randrange
 window.show_ursina_splash = True
 app=Ursina()
 player = FirstPersonController()
-Sky(texture='blue_sky')
-creeper=Entity(model='cube',collider='box',texture='creeper',scale=(2,4,2),position=(5, 3, 2),rotation_y=45)
-creeper.add_script(SmoothFollow(player, offset=(5,1,5)))
+indra=Sky(texture='blue_sky')
+abc=3
+def destroy_creeper():
+    global abc, lives
+    abc=abc-1
+    if abc==0:
+        creeper.color=color.clear
+        creeper.collider=None
+        lives=3
+        creeper.add_script(SmoothFollow(player,offset=[10,10000000,10]))
+creeper=Entity(model='cube',collider='box',texture='creeper',scale=(2,4,2),position=(3, 1, 3),rotation_y=45,on_click=destroy_creeper)
+creeper.add_script(SmoothFollow(player, offset=(1, 1, 1), speed=1))
+
+
 '''for x in range(25):
     for z in range(25):
         y = noise([x * 0.02,z * 0.02])
@@ -15,13 +26,29 @@ creeper.add_script(SmoothFollow(player, offset=(5,1,5)))
 noise = PerlinNoise(octaves=2,seed=randrange(1, 100000000000000000000000000000000000))
 amp = 6
 freq = 24
-
+lives=20
 shells = []
 shellWidth = 12
 def update():
     if player.y < -3:
         player.y = 10
     genTerr()
+    global lives
+    if distance(creeper, player) < 7:
+        lives = lives - 1
+        print('lives-1')
+        if lives == 0:
+            Text('You died!!!', background=True)
+            creeper.color=color.clear
+            creeper.collider=None
+            player.speed=0
+            player.gravity=0
+            mouse.enabled = False
+            mouse.locked = False
+            indra.texture="red_sky"
+            indra.color=color.red
+        creeper.z-=7
+
 for i in range(shellWidth*shellWidth):
     ent = Entity(model='cube', texture='grass', collider='box')
     shells.append(ent)
