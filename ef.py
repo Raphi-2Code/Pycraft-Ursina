76fe017355aea5c925201505c1667a6afb6893f7
Raphi-2Code <70066593+Raphi-2Code@users.noreply.github.com>
2022-02-26 14:41:01 +0100

    Update ef.py

--- ef.py
+++ ef.py
@@ -12,7 +12,7 @@ def destroy_creeper():
     if abc==0:
         creeper.color=color.clear
         creeper.collider=None
-        lives=20
+        #lives=20
         creeper.add_script(SmoothFollow(player,offset=[10,10000000,10]))
         Text('Tasty creeper:)')
 creeper=Entity(model='cube',collider='box',texture='creeper',scale=(2,4,2),position=(3, 1, 3),rotation_y=45,on_click=destroy_creeper)
